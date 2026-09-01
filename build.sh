83b9340b595fbb45d80fea94a390728006e38f42
bunmann <jungd611@gmail.com>
2026-09-01 19:39:43 -0400

    fix: Add requirements.txt to root and week3_fastapi for Render build script

--- build.sh
+++ build.sh
@@ -3,7 +3,11 @@
 set -o errexit
 
 pip install --upgrade pip
-pip install -r week3_fastapi/requirements.txt
+if [ -f "requirements.txt" ]; then
+    pip install -r requirements.txt
+elif [ -f "week3_fastapi/requirements.txt" ]; then
+    pip install -r week3_fastapi/requirements.txt
+fi
 
 cd week4_react
 npm install
